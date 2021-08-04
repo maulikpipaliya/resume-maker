351c8b570ba8b2bdc62ddafa58a0633b96dfc36c
sachinrathodm <sachinrathodm@gmail.com>
2021-08-04 15:45:45 +0530

    RM1040 : Redux: Work Recuder - Added functions

--- src/schema/action-types/IWorkAction.ts
+++ src/schema/action-types/IWorkAction.ts
@@ -1,55 +1,61 @@
-import { IWork } from "../../schema";
+import { IWork } from '../../schema'
 
 export enum WorkActionType {
-    ADD_WORK = "ADD_WORK",
-    UPDATE_WORK = "UPDATE_WORK",
-    DELETE_WORK = "DELETE_WORK",
-    GET_WORKS = "GET_WORKS",
-    GET_WORK_BY_ID = "GET_WORK_BY_ID",
-    RESET_WORK = "RESET_WORK",
+    ADD_WORK = 'ADD_WORK',
+    UPDATE_WORK_BY_INDEX = 'UPDATE_WORK_BY_INDEX',
+    DELETE_WORK = 'DELETE_WORK',
+    GET_WORKS = 'GET_WORKS',
+    GET_WORK_BY_ID = 'GET_WORK_BY_ID',
+    RESET_WORKS = 'RESET_WORKS',
+    UPDATE_WORKS = 'UPDATE_WORKS',
 }
 
 export enum WorkActionErrors {
     WRONG_ID = "Given ID doesn't exist! Please try again with correct parameters",
 }
-
-interface IAddWorkAction {
-    type: WorkActionType.ADD_WORK;
-    payload: IWork;
+interface IUpdateWorkAction {
+    type: WorkActionType.UPDATE_WORKS
+    payload: IWork[]
 }
 
-interface IUpdateWorkAction {
-    type: WorkActionType.UPDATE_WORK;
+interface IAddWorkAction {
+    type: WorkActionType.ADD_WORK
+    payload: IWork
+}
+
+interface IUpdateWorkByIndexAction {
+    type: WorkActionType.UPDATE_WORK_BY_INDEX
     payload: {
-        id: number;
-        work: IWork;
-    };
+        idx: number
+        updaterecord: IWork
+    }
 }
 
 interface IDeleteWorkAction {
-    type: WorkActionType.DELETE_WORK;
-    payload: number;
+    type: WorkActionType.DELETE_WORK
+    payload: number
 }
 
 interface IGetWorksAction {
-    type: WorkActionType.GET_WORKS;
+    type: WorkActionType.GET_WORKS
 }
 
 interface IGetWorkByIDAction {
-    type: WorkActionType.GET_WORK_BY_ID;
+    type: WorkActionType.GET_WORK_BY_ID
     payload: {
-        id: number;
-    };
+        id: number
+    }
 }
 
-interface IResetWorkAction {
-    type: WorkActionType.RESET_WORK;
+interface IResetWorksAction {
+    type: WorkActionType.RESET_WORKS
 }
 
 export type IWorkAction =
     | IAddWorkAction
-    | IUpdateWorkAction
+    | IUpdateWorkByIndexAction
     | IDeleteWorkAction
     | IGetWorksAction
     | IGetWorkByIDAction
-    | IResetWorkAction;
+    | IResetWorksAction
+    | IUpdateWorkAction
