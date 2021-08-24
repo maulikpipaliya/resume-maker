e0ecb79eb78ee83b4d2c1186b7051ddffd629048
Maulik Pipaliya <maulik.pipaliya@gmail.com>
2021-08-24 10:16:41 +0530

    Added Script and Config Changes

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,8 +1,8 @@
 //application level configuration
+import dotenv from "dotenv"
+dotenv.config()
 export const config = {
-    port: process.env.PORT || 3000,
-    baseURL: process.env.BASE_URL || "http://localhost:3000",
-    dbURL:
-        process.env.MONGO_URI ||
-        "mongodb+srv://rm-maulik:L3wHUK89XWLl5zUb@db-resume-maker.xjflx.mongodb.net/db-resume-maker",
+    port: process.env.PORT,
+    baseURL: process.env.BASE_URL,
+    dbURL: process.env.MONGO_URI,
 }
---
+++ startApp.bat
@@ -0,0 +1,2 @@
+start powershell -noexit -command "cd server;npm run start";
+start powershell -noexit -command "npm run start";
