0286345f9645ef302fc2109741dcfd6e8a0f8f1b
sachinrathodm <sachinrathodm@gmail.com>
2021-08-27 15:20:15 +0530

    api  for set Data

--- server/src/api/controllers/resume.controller.js
+++ server/src/api/controllers/resume.controller.js
@@ -2,26 +2,23 @@ import userModel from "../../models/userModel.js"
 import asyncHandler from "express-async-handler"
 
 export const setBasics = asyncHandler(async (req, res) => {
-    const { data, authEmail, type } = req.body
-    if (!(data && authEmail && type))
-        return res.status(401).send("Missing Fields.")
+    const { data, authEmail } = req.body
+    if (!(data && authEmail)) return res.status(401).send("Missing Fields.")
     try {
         const user = await userModel.findOne({ authEmail })
-        //console.log(user)
-        console.log(data[0][type])
         if (!user) return res.status(401).send("User not Found.")
-
-        Object.keys(data[0][type]).forEach(async (key) => {
-            if (!user.data[0]) user.data[0] = data[0]
-            else user.data[0][type][key] = data[0][type][key]
+        Object.keys(data[0]).forEach(async (schema) => {
+            Object.keys(data[0][schema]).forEach(async (key) => {
+                if (!user.data[0]) user.data[0] = data[0]
+                else user.data[0][schema][key] = data[0][schema][key]
+            })
         })
         const response = await user.save()
-        if (!response) res.status(400).json("Failed to update")
-        else res.status(201).json(response)
+        if (!response) return res.status(400).json("Failed to update")
+        else return res.status(201).json(response)
 
         console.log(element)
     } catch (err) {
-        // console.log(err)
-        res.status(400).send(err)
+        return res.status(400).send(err)
     }
 })
